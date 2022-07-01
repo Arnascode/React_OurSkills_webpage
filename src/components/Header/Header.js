75e3d719cdfa8f642b0e29a9b9d8b52a21d5c635
Arnascode <vilkoncius7@gmail.com>
2022-07-01 23:27:27 +0300

    head

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,38 @@
 import { Link, NavLink } from 'react-router-dom';
 import { useAuthCtx } from '../../store/authContext';
+import css from './Header.module.css';
 
 function Header(props) {
   const { isUserLoggedIn, logout, userEmail } = useAuthCtx();
 
   return (
-    <header>
-      <nav className='navbar navbar-expand-sm navbar-dark bg-dark '>
-        <div className='container'>
-          <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-            <div className='navbar-nav ml-auto'>
+    <header className={css.header}>
+      <nav>
+        <div>
+          <div>
+            <div>
               <NavLink className='nav-link' exact to={'/'}>
-                <img src='./img/img_flwr.gif' alt='' srcset='' />
+                <img className={css.img} src='./img/img_flwr.gif' alt='' srcset='' />
               </NavLink>
               {isUserLoggedIn && (
                 <>
-                  <NavLink className='nav-link' exact to={'/'}>
+                  <NavLink className={css['nav-link']} exact to={'/'}>
                     Home
                   </NavLink>
-                  <NavLink className='nav-link' to={'/posts'}>
+                  <NavLink className={css['nav-link']} to={'/posts'}>
                     Posts
                   </NavLink>
-                  <NavLink onClick={logout} className='nav-link' to={'/login'}>
+                  <NavLink onClick={logout} className={css['nav-link']} to={'/login'}>
                     Logout
                   </NavLink>
                 </>
               )}
               {!isUserLoggedIn && (
                 <>
-                  <NavLink className='nav-link' to={'/login'}>
+                  <NavLink className={css['nav-link']} to={'/login'}>
                     Login
                   </NavLink>
-                  <NavLink className='nav-link' to={'/register'}>
+                  <NavLink className={css['nav-link']} to={'/register'}>
                     Register
                   </NavLink>
                 </>
--- src/store/authContext.js
+++ src/store/authContext.js
@@ -12,19 +12,19 @@ const AuthContext = createContext({
 AuthContext.displayName = 'AuthContext';
 
 const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem('posts-token'));
+  const [token, setToken] = useState(localStorage.getItem('token'));
   const [userEmail, setUserEmail] = useState('');
 
   const login = (gotToken, gotEmail) => {
     // console.log('gotToken ===', gotToken);
     setToken(gotToken);
     setUserEmail(gotEmail);
-    localStorage.setItem('posts-token', gotToken);
+    localStorage.setItem('token', gotToken);
   };
   const logout = () => {
     setToken(null);
     setUserEmail(null);
-    localStorage.removeItem('posts-token');
+    localStorage.removeItem('token');
   };
 
   const ctx = {
